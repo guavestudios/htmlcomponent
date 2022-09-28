55476285d9becca2d635af4222b7b1d4b44c64e4
nomiad <nomiadcode@gmail.com>
2022-09-28 15:13:45 +0200

    added module definition to rollup

--- rollup.config.js
+++ rollup.config.js
@@ -2,11 +2,13 @@ import { terser } from "rollup-plugin-terser";
 
 export default {
   input: 'src/htmlcomponent.js',
-	output: {
-		file: 'dist/htmlcomponent.min.js',
-		format: 'umd',
-		name: 'htmlcomponent'
-	},
+	output: [
+		{
+			file: 'dist/htmlcomponent.min.js',
+			format: 'umd',
+			name: 'htmlcomponent'
+		}
+	],
 	plugins: [
 		terser()
 	]
